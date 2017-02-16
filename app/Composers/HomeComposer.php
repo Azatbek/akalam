124fee66baf348e3765b404e85438818cf7b4d17
Tumenbayev <adilbek.tumenbayev@gmail.com>
2017-02-16 11:39:06 +0600

    Fix #1

---
+++ app/Composers/HomeComposer.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Composers;
+use App\Models\News;
+class HomeComposer
+{
+    public function compose($view)
+    {
+        $view->with('news' => News::getNews(6));
+        $view->with('categories' => Category::getCategories());
+    }
+}
+
+?>
